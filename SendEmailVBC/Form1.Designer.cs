0aec36690099890921e73738190a879d42222668
thinhtvdn <70940314+thinhtvdn@users.noreply.github.com>
2024-09-29 20:51:46 +0700

    add1

---
+++ SendEmailVBC/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SendEmailVBC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.txtSoLuongNV = new System.Windows.Forms.TextBox();
+            this.txtFileDuLieu = new System.Windows.Forms.TextBox();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.btExcel = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btDong = new System.Windows.Forms.Button();
+            this.oGrid = new System.Windows.Forms.DataGridView();
+            this.cmbGV = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.oGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenFileDialog1
+            // 
+            this.OpenFileDialog1.FileName = "OpenFileDialog1";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label6.Location = new System.Drawing.Point(1028, 22);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(139, 20);
+            this.Label6.TabIndex = 50;
+            this.Label6.Text = "Số dòng dữ liệu";
+            // 
+            // txtSoLuongNV
+            // 
+            this.txtSoLuongNV.Location = new System.Drawing.Point(1184, 22);
+            this.txtSoLuongNV.Name = "txtSoLuongNV";
+            this.txtSoLuongNV.Size = new System.Drawing.Size(88, 26);
+            this.txtSoLuongNV.TabIndex = 49;
+            // 
+            // txtFileDuLieu
+            // 
+            this.txtFileDuLieu.Location = new System.Drawing.Point(1184, 54);
+            this.txtFileDuLieu.Name = "txtFileDuLieu";
+            this.txtFileDuLieu.Size = new System.Drawing.Size(449, 26);
+            this.txtFileDuLieu.TabIndex = 52;
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label7.ForeColor = System.Drawing.Color.Black;
+            this.Label7.Location = new System.Drawing.Point(1034, 56);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(133, 21);
+            this.Label7.TabIndex = 51;
+            this.Label7.Text = "Tập tin dữ liệu";
+            // 
+            // btExcel
+            // 
+            this.btExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExcel.Location = new System.Drawing.Point(16, 8);
+            this.btExcel.Name = "btExcel";
+            this.btExcel.Size = new System.Drawing.Size(279, 55);
+            this.btExcel.TabIndex = 53;
+            this.btExcel.Text = "Lấy dữ liệu từ tập tin EXCEL";
+            this.btExcel.UseVisualStyleBackColor = true;
+            this.btExcel.Click += new System.EventHandler(this.btExcel_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(303, 8);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(279, 55);
+            this.btnCheck.TabIndex = 54;
+            this.btnCheck.Text = "Kiểm tra trùng lịch";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btDong
+            // 
+            this.btDong.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDong.Location = new System.Drawing.Point(588, 8);
+            this.btDong.Name = "btDong";
+            this.btDong.Size = new System.Drawing.Size(75, 55);
+            this.btDong.TabIndex = 56;
+            this.btDong.Text = "Đóng";
+            this.btDong.UseVisualStyleBackColor = true;
+            this.btDong.Click += new System.EventHandler(this.btDong_Click);
+            // 
+            // oGrid
+            // 
+            this.oGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.oGrid.Location = new System.Drawing.Point(0, 256);
+            this.oGrid.Name = "oGrid";
+            this.oGrid.RowHeadersWidth = 62;
+            this.oGrid.Size = new System.Drawing.Size(1633, 563);
+            this.oGrid.TabIndex = 42;
+            // 
+            // cmbGV
+            // 
+            this.cmbGV.FormattingEnabled = true;
+            this.cmbGV.Location = new System.Drawing.Point(158, 82);
+            this.cmbGV.Name = "cmbGV";
+            this.cmbGV.Size = new System.Drawing.Size(312, 28);
+            this.cmbGV.TabIndex = 57;
+            this.cmbGV.SelectedIndexChanged += new System.EventHandler(this.cmbGV_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Danh sách GV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1645, 819);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbGV);
+            this.Controls.Add(this.btDong);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btExcel);
+            this.Controls.Add(this.txtFileDuLieu);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.txtSoLuongNV);
+            this.Controls.Add(this.oGrid);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.oGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.OpenFileDialog OpenFileDialog1;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.TextBox txtSoLuongNV;
+        internal System.Windows.Forms.TextBox txtFileDuLieu;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Button btExcel;
+        internal System.Windows.Forms.Button btnCheck;
+        internal System.Windows.Forms.Button btDong;
+        internal System.Windows.Forms.DataGridView oGrid;
+        private System.Windows.Forms.ComboBox cmbGV;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SendEmailVBC/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Reflection;
+
+
+namespace SendEmailVBC
+{
+    public partial class Form1 : Form
+    {
+        DataSet oDs = new DataSet();
+        string tenTableThongtinGV = "lichBaoVeGV";
+        string tencotGT1 = "Giám thị 1";
+        string tencotGT2 = "Giám thị 2";
+        string tenCotNgayCoiThi = "Ngày";
+        string tencotCaLamViec = "Ca học";
+        string tenCotSTT = "STT"; // STT dùng để tô màu dòng bị trùng
+        int sttColor = 0; // Đếm số thứ tự GV, stt GV hiện tại -> lấy màu 
+
+        DataView dataView; // dùng để lọc dữ liệu theo giám thị 1
+        DataTable tblData;
+
+        private bool isLoaded = false; // Cờ để theo dõi trạng thái form, liên quan đến SelectedIndexChanged cmbGV 
+        string cmbGVTieuDe = "Xem tất cả";
+
+
+        // Tạo một HashSet lưu các phần tử là stt các hàng bị trùng lịch
+        HashSet<double> dsTrungLich = new HashSet<double>();
+
+
+        string[] MangCotThongTin;
+        string[] MangTieuDeCotThongTin;
+        int soLuongNV,soCot, soDong;
+
+
+        private string GetColor(int index)
+        {
+            // Danh sách màu sắc  
+            string[] colors = {
+            "LightGreen"
+        };
+            return colors[index % colors.Length]; // Lặp lại màu sắc nếu vượt quá số màu
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void XoaLuoiDuLieu()
+        {
+            oDs.Tables.Clear();
+            oGrid.DataSource = null;
+        }
+
+        private void btDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private List<string> layDanhSachGv()
+        {
+            // Bước - lấy danh sách tất cả giảng viên
+            var danhSachGiamThi = oDs.Tables[tenTableThongtinGV]
+            .AsEnumerable() // Chuyển đổi thành Enumerable
+            .Select(row => row.Field<string>(tencotGT1)) // Lấy giá trị từ cột "Giám thị 1"
+            .Concat(oDs.Tables[tenTableThongtinGV] // Kết hợp với giá trị từ cột "Giám thị 2"
+                .AsEnumerable()
+                .Select(row => row.Field<string>(tencotGT2)))
+            .Distinct() // Lọc ra các giá trị không trùng
+            .ToList(); // Chuyển đổi thành danh sách
+
+            return danhSachGiamThi;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            dsTrungLich.Clear(); // danh sách stt các dòng bị trùng lịch
+
+            // Bước - lấy danh sách tất cả giảng viên
+            var danhSachGiamThi = oDs.Tables[tenTableThongtinGV]
+            .AsEnumerable() // Chuyển đổi thành Enumerable
+            .Select(row => row.Field<string>(tencotGT1)) // Lấy giá trị từ cột "Giám thị 1"
+            .Concat(oDs.Tables[tenTableThongtinGV] // Kết hợp với giá trị từ cột "Giám thị 2"
+                .AsEnumerable()
+                .Select(row => row.Field<string>(tencotGT2)))
+            .Distinct() // Lọc ra các giá trị không trùng
+            .ToList(); // Chuyển đổi thành danh sách
+                       // Bước - kiểm tra
+                       //      Với mổi giảng viên trong danh sách 
+                       //      Chọn toàn bộ các dòng mà giảng viên là giám thị 1 hay giám thị 2
+                       //        báo lổi nếu :  Với mổi ngày trong danh sách của giảng viên đã chọn, có 2 ca trùng nhau  
+            foreach (var gt1 in danhSachGiamThi)
+            {
+                string colorGV = GetColor(sttColor); // lấy màu của GV thứ sttColor
+
+                // Lấy danh sách các hàng thỏa mãn điều kiện và nhóm theo ngày và ca làm việc
+                var groupedKiemTra = oDs.Tables[tenTableThongtinGV]
+                    .AsEnumerable() // Chuyển đổi thành Enumerable
+                    .Select(row => new
+                    {
+                        Ngay = row.Field<DateTime>(tenCotNgayCoiThi),
+                        CaLamViec = row.Field<double>(tencotCaLamViec),
+                        GiamThi1 = row.Field<string>(tencotGT1),
+                        GiamThi2 = row.Field<string>(tencotGT2),
+                        STT = row.Field<double>(tenCotSTT)
+                    })
+                    .Where(item => item.GiamThi1 == gt1 || item.GiamThi2 == gt1) // Lọc ngay sau khi chọn
+                    .GroupBy(item => new
+                    {
+                        item.Ngay,
+                        item.CaLamViec
+                    }) // Nhóm theo ngày và ca làm việc
+                    .Select(group => new
+                    {
+                        Ngay = group.Key.Ngay, // Lấy ngày từ khóa của nhóm
+                        CaLamViec = group.Key.CaLamViec, // Lấy ca làm việc từ khóa của nhóm
+                        GiamsThi = group.Select(g => new
+                        {
+                            GiamThi1 = g.GiamThi1, // Lấy giá trị giám thị 1 từ từng phần tử trong nhóm
+                            GiamThi2 = g.GiamThi2,  // Lấy giá trị giám thị 2 từ từng phần tử trong nhóm
+                            STT = g.STT
+                        }).ToList() // Chuyển đổi danh sách giám thị thành một danh sách
+                    })
+                    .ToList(); // Chuyển đổi kết quả cuối cùng thành danh sách
+                // Lọc và lấy các nhóm có số lượng dòng chi tiết > 1 -> bị trùng 
+                var filteredGroups = groupedKiemTra
+                    .Where(group => group.GiamsThi.Count > 1) // Chọn nhóm có số lượng dòng chi tiết > 1
+                    .ToList();
+                // Lấy STT các dòng bị trùng ngày + ca làm việc -> tô màu nền 
+                foreach (var group in filteredGroups)
+                    foreach (var giamThi in group.GiamsThi)
+                    {
+                        dsTrungLich.Add(giamThi.STT);
+                        ToMauGrid(giamThi.STT, colorGV); // -> tô màu nền , mổi giám thị 1 màu khác nhau
+                    }
+                sttColor++; // giảng viên tiếp theo
+            }
+        }
+
+        private void ToMauGrid(double sttDong,string colorGV)
+        {
+            foreach (DataGridViewRow row in oGrid.Rows)
+            {
+                // Kiểm tra xem giá trị trong cột STT có bằng targetSTT không
+                if (row.Cells["STT"].Value != null && row.Cells["STT"].Value.ToString() == sttDong.ToString())
+                {
+                    // Tô màu nền đỏ cho dòng này
+                    row.DefaultCellStyle.BackColor = System.Drawing.Color.FromName(colorGV);
+                    // Dừng vòng lặp ngay sau khi đã tô màu cho một dòng
+                    break;
+                }
+            }
+        }
+
+      
+        private void btExcel_Click(object sender, EventArgs e)
+        {
+            {
+                string ConnectionString;
+                string tenFile;
+
+                // /////////////// chon tap tin 
+                OpenFileDialog1.Filter = "Tập tin Excel (*.xls;*.xlsx)|*.xls;*.xlsx";
+                OpenFileDialog1.Title = " Chọn tập tin dữ liệu ";
+                OpenFileDialog1.FileName = "";
+                if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    txtFileDuLieu.Text = OpenFileDialog1.FileName;
+                }
+                else
+                {
+                    return;
+                }
+                // ////////////////////////////////////////////////////////
+                XoaLuoiDuLieu();
+                tenFile = txtFileDuLieu.Text; // tenFile = Application.StartupPath & "\" & "dataExcel.xls"
+                // Kiểm tra định dạng file
+                if (tenFile.EndsWith(".xls")) // phương thức của lớp string,kiểm tra xem một chuỗi có kết thúc bằng một chuỗi khác hay không
+                {
+                    ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + tenFile + ";Extended Properties='Excel 8.0;HDR=YES';";
+                }
+                else if (tenFile.EndsWith(".xlsx"))
+                {
+                    ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + tenFile + ";Extended Properties='Excel 12.0 Xml;HDR=YES';";
+                }
+                else
+                {
+                    throw new Exception("Định dạng file không hợp lệ. Chỉ hỗ trợ .xls và .xlsx.");
+                }
+
+                using (OleDbConnection oCnn = new OleDbConnection(ConnectionString))
+                {
+                    try
+                    {
+                        oCnn.Open();
+                        // Lấy tên bảng đầu tiên
+                        DataTable dtSchema = oCnn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                        string firstSheetName = dtSchema.Rows[0]["TABLE_NAME"].ToString();
+                        // Đọc dữ liệu từ bảng đầu tiên
+                        string query = $"SELECT * FROM [{firstSheetName}]";
+                        using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, oCnn))
+                        {
+                            oDs.Clear();
+                            adapter.Fill(oDs, tenTableThongtinGV);
+                            tblData = oDs.Tables[tenTableThongtinGV];
+                            // Sắp xếp dữ liệu theo cột ngày
+                            dataView = new DataView(tblData); 
+                            dataView.Sort = tenCotNgayCoiThi +  " ASC, " + tencotCaLamViec + " ASC";
+                            // Gán DataView làm nguồn dữ liệu cho GridView
+                            oGrid.DataSource = dataView;
+
+                            soLuongNV = oGrid.RowCount - 1;  // không đếm dòng tiêu đề
+                            txtSoLuongNV.Text = soLuongNV.ToString();
+                            soCot = oGrid.Columns.Count; // chu y, cot sẽ bat dau tu 0
+                            soDong = oGrid.Rows.Count - 1; // dong tren luoi luôn có 1 dòng empty bên dưới
+                                                           //chu y, dong sẽ bat dau tu 0 (trong do cos 1 dong tieu de trong file Excel)
+                                                           // có 1 dòng là bắt đầu từ 0 đến 0
+
+
+
+                            // Gọi hàm để lấy danh sách giám thị
+                            var danhSachGiamThi = layDanhSachGv();
+                            // Tạo danh sách mới và thêm "Chọn" vào đầu
+                            var danhSachComboBox = new List<string> { cmbGVTieuDe }; // Thêm dòng "Chọn"
+                            danhSachComboBox.AddRange(danhSachGiamThi); // Thêm các giá trị từ danh sách giám thị
+                            // Gán danh sách vào ComboBox
+                            cmbGV.DataSource = danhSachComboBox;
+
+                            // Định dạng hiển thị sau khi đã load dữ liệu cho lưới
+                            oGrid.ScrollBars = ScrollBars.Both; // Hoặc ScrollBars.Vertical hoặc ScrollBars.Horizontal
+                            oGrid.Columns[tenCotNgayCoiThi].DefaultCellStyle.Format = "dd/MM/yyyy";
+                            oGrid.Columns[tenCotSTT].DefaultCellStyle.Format = "N0";
+                            oGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Lỗi: " + ex.Message);
+                    }
+                }
+
+           
+
+            }
+            isLoaded = true; // Đặt cờ là true khi form đã tải xong, ogrid đã có dữ liệu
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Kiểm tra nếu phím nhấn là Esc
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close(); // Đóng form
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            dieuChinhKichThuocForm();
+        }
+
+        private void cmbGV_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Lấy giá trị đã chọn từ ComboBox
+            string giaTriChon = cmbGV.SelectedItem?.ToString();
+            // Kiểm tra nếu giá trị là "Chọn"
+            if (string.IsNullOrEmpty(giaTriChon) || giaTriChon == cmbGVTieuDe)
+            {
+                dataView.RowFilter = ""; // Thiết lập RowFilter thành chuỗi rỗng để xem tất cả
+                dataView.Sort = tenCotNgayCoiThi + " ASC"; // Sắp xếp dữ liệu theo cột ngày
+                oGrid.DataSource = dataView;
+                return; // Không thực hiện thêm bất kỳ hành động nào
+            }
+
+            // Lọc dữ liệu theo giá trị đã chọn
+            if (!string.IsNullOrEmpty(giaTriChon))
+            {
+                // Thiết lập DataView với bộ lọc
+                string dieuKienLoc = "[" + tencotGT1 + "] = '" + giaTriChon+"'";
+                dataView.RowFilter = dieuKienLoc; // Thay đổi tenCotGiua thành tên cột cần lọc
+                dataView.Sort = tenCotNgayCoiThi + " ASC"; // Sắp xếp dữ liệu theo cột ngày
+                oGrid.DataSource = dataView;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dieuChinhKichThuocForm()
+        {
+            // Tính toán chiều cao và chiều rộng mới
+            int newHeight = this.Height; // Chiều cao bằng chiều cao của form 
+            int newWidth = this.Width; // Chiều rộng bằng chiều rộng của form
+
+            // Cập nhật kích thước cho GridView
+            oGrid.Height = newHeight-150;
+            oGrid.Width = newWidth;
+
+            // Đặt vị trí của DataGridView để đáy nằm sát đáy của form
+            oGrid.Top = this.ClientSize.Height - oGrid.Height;  // Đặt vị trí trên cùng
+        }
+
+    }
+}
